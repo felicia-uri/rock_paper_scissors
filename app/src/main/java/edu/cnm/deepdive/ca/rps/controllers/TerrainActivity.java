5ccba6bfce1f64f41cbd4efa9b81596a6e6a1234
felicia-uri <felicia.uri@gmail.com>
2017-10-23 08:14:38 -0600

    Added Java Docs to TerrainActivity.java. Added info-package.java

--- app/src/main/java/edu/cnm/deepdive/ca/rps/controllers/TerrainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/ca/rps/controllers/TerrainActivity.java
@@ -1,7 +1,7 @@
 package edu.cnm.deepdive.ca.rps.controllers;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -9,10 +9,18 @@ import edu.cnm.deepdive.ca.rps.R;
 import edu.cnm.deepdive.ca.rps.models.Terrain;
 import edu.cnm.deepdive.ca.rps.views.TerrainView;
 
+/**
+ * Activity class for Deadlock(Rock-Paper-Scissors) cellular automaton.
+ * @author FeliciaUrioste
+ */
 public class TerrainActivity extends AppCompatActivity {
 
+  /** Default rest duration of overridden run method of Thread class inside nested Runner class */
   private static final int RUNNER_THREAD_REST = 40;
+
+  /** Default sleep duration of overridden run method of Thread class inside nested Runner class */
   private static final int RUNNER_THREAD_SLEEP = 50;
+
 
   private boolean running = false;
   private boolean inForeground = false;
@@ -30,12 +38,6 @@ public class TerrainActivity extends AppCompatActivity {
   }
 
   @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    getMenuInflater().inflate(R.menu.options, menu);
-    return super.onCreateOptionsMenu(menu);
-  }
-
-  @Override
   protected void onStart() {
     super.onStart();
     setInForeground(true);
@@ -45,6 +47,12 @@ public class TerrainActivity extends AppCompatActivity {
   protected void onStop() {
     setInForeground(false);
     super.onStop();
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.options, menu);
+    return super.onCreateOptionsMenu(menu);
   }
 
   @Override
@@ -77,29 +85,64 @@ public class TerrainActivity extends AppCompatActivity {
     return true;
   }
 
+  /**
+   * Instantiates a new Terrain object with a reference name terrain.
+   * Calls the initialize() method inherited from Terrain model to create a lattice with
+   * randomly assigned instance of Breed.
+   */
   private void initializeModel() {
     terrain = new Terrain();
     terrain.initialize();
   }
 
+  /**
+   * Assign values for terrainLayout and terrainView objects.
+   *
+   */
   private void initializeUserInterface() {
     terrainLayout = findViewById(R.id.terrainLayout);
     terrainView = (TerrainView) findViewById(R.id.terrainView);
   }
 
-  public boolean isRunning() {
+  /**
+   * Returns the currently specified value of running field.
+   *
+   * @return on or off flag
+   */
+  private synchronized boolean isRunning() {
     return running;
   }
 
-  public void setRunning(boolean running) {
+  /**
+   * Sets the value of running to be used by the Runner class to determine whether to run
+   * terrain.step() and terrainView.setSource() methods.
+   *
+   * Allows the View to be updated or not based on the two conditions specified in
+   * the while loop inside the Runner class.
+   *
+   * @param running on or off flag
+   */
+  private synchronized void setRunning(boolean running) {
     this.running = running;
   }
 
-  public boolean isInForeground() {
+  /**
+   * Returns the currently specified value of inForeground field based whether the app is paused,
+   * running or resetting.
+   *
+   * @return on or off flag
+   */
+  private synchronized boolean isInForeground() {
     return inForeground;
   }
 
-  public void setInForeground(boolean inForeground) {
+  /**
+   * Sets the value of inForeground to be used as a flag to determine whether to reset, pause, or
+   * run the app.
+   *
+   * @param inForeground on or off flag
+   */
+  private synchronized void setInForeground(boolean inForeground) {
     if (inForeground) {
       this.inForeground = true;
       if (runner == null) {
@@ -118,6 +161,7 @@ public class TerrainActivity extends AppCompatActivity {
     }
   }
 
+  // TODO - Understand what the Runner class is doing
   private class Runner extends Thread {
 
     @Override
@@ -129,15 +173,17 @@ public class TerrainActivity extends AppCompatActivity {
           try {
             Thread.sleep(RUNNER_THREAD_REST);
           } catch (InterruptedException ex) {
-            //Do nothing.
+            // Do nothing.
           }
         }
         try {
           Thread.sleep(RUNNER_THREAD_SLEEP);
         } catch (InterruptedException ex) {
-          //Do nothing.
+          // Do nothing.
         }
       }
     }
   }
+
 }
+
---
+++ app/src/main/java/edu/cnm/deepdive/ca/rps/controllers/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Classes that implement a Rock-Paper-Scissor (RPS) controller of an ecosystem with cyclic dominance between
+ * 3 species. This control methodology allows for the smooth communication and coordination
+ * between the view and the model structure of this app.
+ *
+ * @author Ejerson Balabas
+ *
+ * @version 1.0, 2017-10-20
+ */
+package edu.cnm.deepdive.ca.rps.controllers;
